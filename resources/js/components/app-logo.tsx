6f7422609a8801606aa9cd0c8a6ef69ce9861d01
guevarafranklin <tecguevara@gmail.com>
2025-06-16 20:08:36 -0600

    added the logo but without seeing it in the website

--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -4,7 +4,7 @@ export default function AppLogo() {
     return (
         <>
             <div className="bg-linear-to-r from-amber-500 to-amber-600 text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-md">
-                <AppLogoIcon className="size-5 fill-current text-white dark:text-black" />
+                <AppLogoIcon />
             </div>
             <div className="ml-1 grid flex-1 text-left text-sm">
                 <span className="mb-0.5 truncate leading-none font-semibold text-amber-600">Virtopus Agency</span>
